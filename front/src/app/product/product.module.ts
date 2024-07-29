5f8b850841ee6c54f5c54147cf5f539521b30a8f
Shan <theo.pessegue2@gmail.com>
2024-07-29 10:14:19 +0200

    feat : admin page

--- front/src/app/product/product.module.ts
+++ front/src/app/product/product.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductsAdminComponent } from './products-admin/products-admin.component';
 import { ProductsComponent } from './products/products.component';
-
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +13,8 @@ import { ProductsComponent } from './products/products.component';
   ],
   imports: [
     CommonModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    SharedModule,
   ]
 })
 export class ProductModule { }
--- front/src/app/product/product.service.ts
+++ front/src/app/product/product.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private productsUrl = 'assets/products.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<any> {
+    return this.http.get<any>(this.productsUrl);
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.productsUrl}/${id}`);
+  }
 }
